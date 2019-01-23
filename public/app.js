b3da2efff4f4475d1bfad651e434c8facc125adb
meron93 <meronyemane18@gmail.com>
2019-01-23 15:18:28 -0800

    user management project added to portfolio

--- public/app.js
+++ public/app.js
@@ -2,12 +2,17 @@ $(document).ready(function() {
   $(window).scroll(function() {
     var quoteCatcherDivToptoBottom = $("#quote-catcher-project").offset().top + $("#quote-catcher-project")[0].scrollHeight;
     var diskovDivTopToBottom = $("#diskov-project").offset().top + $("#diskov-project")[0].scrollHeight;
+    var usermanagementDivTopToBottom = $("#usermanagement-project").offset().top + ("#usermanagment-project")[0].scrollHeight;
     if ($(window).scrollTop() > $("#quote-catcher-project").offset().top - $(window).height()) {
       $("#quote-catcher-project").addClass("animated slideInLeft");
     }
 
     if ($(window).scrollTop() > $("#diskov-project").offset().top - $(window).height()) {
       $("#diskov-project").addClass("animated slideInRight");
+    }
+
+    if ($(window).scrollTop() > $("#usermanagement-project").offset().top - $(window).height()) {
+      $("#usermanagement-project").addClass("animated slideInLeft");
     }
   });
 
